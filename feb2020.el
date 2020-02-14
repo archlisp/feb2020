483daa644ec8306e62645ec280a5c83e4a1db64d
Travis <tssund93@gmail.com>
2020-02-13 19:47:20 -0600

    init

---
+++ feb2020.el
@@ -0,0 +1,10 @@
+;;; feb2020.el --- Emacs stuff -*- lexical-binding: t; -*-
+;;; feb2020.el -- Summary:
+;; Package-Requires: ((dash "2.17.0") (dash-functional "1.2.0") (emacs "24") (s "1.12.0") (ht "2.3"))
+;;; Commentary:
+;;; Code:
+
+
+
+;;; feb2020.el ends here
+(provide 'feb2020)
